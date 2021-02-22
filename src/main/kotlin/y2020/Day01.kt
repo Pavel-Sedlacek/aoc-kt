029299738852a00062781905235241925bd254bb
pavel <pavel.sedlacek2004@gmail.com>
2021-02-22 13:49:35 +0100

    Day01.kt + Day02.kt

---
+++ src/main/kotlin/y2020/Day01.kt
@@ -0,0 +1,39 @@
+package y2020
+
+import utils.*
+
+class Day01: Day {
+
+    private val input = Utils.readFileAsInts(2020, 1)
+
+    override fun runAll() {
+        println("Day 01 : find product of two/three numbers that sums to 2020")
+        this.partOne(input)
+        this.partTwo(input)
+    }
+
+    private fun partOne(x: List<Int>): Int {
+        for (n1 in x) {
+            if (n1 > 2020) continue
+            for (n2 in x)
+                if (n1 + n2 == 2020)
+                    return n1 * n2
+        }
+        return 0
+    }
+
+    private fun partTwo(x: List<Int>): Int {
+        for (n1 in x) {
+            if (n1 > 2020) continue
+            for (n2 in x) {
+                if (n1 + n2 > 2020) continue
+                for (n3 in x)
+                    if (n1 + n2 + n3 == 2020)
+                        return n1 * n2 * n3
+            }
+        }
+        return 0
+    }
+
+}
+
---
+++ src/main/kotlin/y2020/Day02.kt
@@ -0,0 +1,36 @@
+package y2020
+
+import utils.Day
+import utils.Utils
+
+class Day02: Day {
+
+    private val input = Utils.readFileAsLines(2020, 2)
+
+    override fun runAll() {
+        println("Day 02 : check if letter occurrences in string matches range")
+        println(partOne(input))
+        println(partTwo(input))
+    }
+
+    private fun partOne(x: List<String>): Int =
+        x.count { i ->
+            val min = i.substringBefore("-").toInt()
+            val max = i.substringAfter("-").substringBefore(" ").toInt()
+            val letter = i.substringAfter(" ").dropLast(1)
+            val password = i.substringAfter(": ")
+            (password.count { letter.contains(it) } in min..max)
+        }
+
+    private fun partTwo(x: List<String>): Int =
+        x.count { i ->
+            val min = i.substringBefore("-").toInt()
+            val max = i.substringAfter("-").substringBefore(" ").toInt()
+            val letter = i.substringAfter(" ").dropLast(1)
+            val password = i.substringAfter(": ")
+            (password[min - 1] == letter[0] && password[max - 1] != letter[0]) || (password[min - 1] != letter[0] && password[max - 1] == letter[0])
+        }
+
+
+
+}
