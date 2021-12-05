cc6a74eef990ad1c59cdb635562bf29a5e68fdef
pavel <pavel.sedlacek2004@gmail.com>
2021-12-05 21:03:15 +0100

    prepared days

---
+++ src/main/kotlin/y2021/Day21.kt
@@ -0,0 +1,18 @@
+package y2021
+
+import utils.Day
+
+class Day21 : Day<Int> {
+
+    val input = file
+
+    override fun runAll() = super.run({ partOne(input) }, { partTwo(input) })
+
+    private fun partOne(input: String): Int {
+       return 0 
+    }
+
+    private fun partTwo(input: String): Int {
+       return 0 
+    }
+}
---
+++ src/main/kotlin/y2022/Day17.kt
@@ -0,0 +1,18 @@
+package y2022
+
+import utils.Day
+
+class Day17 : Day<Int> {
+
+    val input = file
+
+    override fun runAll() = super.run({ partOne(input) }, { partTwo(input) })
+
+    private fun partOne(input: String): Int {
+       return 0 
+    }
+
+    private fun partTwo(input: String): Int {
+       return 0 
+    }
+}
