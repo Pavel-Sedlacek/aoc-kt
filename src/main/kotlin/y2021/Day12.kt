8568e823b3dbf8dc06803b8128545ad7f4565c2c
pavel <pavel.sedlacek2004@gmail.com>
2021-12-12 10:23:40 +0100

    2021/12

--- src/main/kotlin/y2021/Day12.kt
+++ src/main/kotlin/y2021/Day12.kt
@@ -1,18 +1,40 @@
 package y2021
 
 import utils.Day
+import utils.readers.asLines
 
 class Day12 : Day<Int> {
 
-    val input = file
-
-    override fun runAll() = super.run({ partOne(input) }, { partTwo(input) })
-
-    private fun partOne(input: String): Int {
-       return 0 
+    val input = mutableMapOf<String, Set<String>>().withDefault { setOf() }.apply {
+        file.trim().asLines().map { it.trim().split("-") }.forEach { (a, b) ->
+            put(a, getValue(a) + b)
+            put(b, getValue(b) + a)
+        }
     }
 
-    private fun partTwo(input: String): Int {
-       return 0 
+    override fun runAll() {
+        super.run({ partOne(input) }, { partTwo(input) })
+    }
+
+    private fun partOne(input: MutableMap<String, Set<String>>): Int = Route(input, false).allPaths.size
+
+    private fun partTwo(input: MutableMap<String, Set<String>>): Int = Route(input, true).allPaths.size
+}
+
+class Route(private val connections: MutableMap<String, Set<String>>, private val canVisitSmallTwice: Boolean) {
+    val allPaths = search("start", listOf())
+
+    private fun search(curr: String, path: List<String>): List<List<String>> {
+        val updatedPath = path + curr
+        if (curr == "end") return listOf(updatedPath)
+        return connections.getValue(curr).filterNot { next ->
+            next == "start" ||
+                    next.isSmallCave() && next in updatedPath &&
+                    if (canVisitSmallTwice) {
+                        updatedPath.filter { it.isSmallCave() }.groupingBy { it }.eachCount().values.any { it >= 2 }
+                    } else true
+        }.flatMap { search(it, updatedPath) }
     }
 }
+
+fun String.isSmallCave() = this.all { it.isLowerCase() }
