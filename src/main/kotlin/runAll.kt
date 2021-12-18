c3658a8e65e3a2c5323e33b3aa45685da8c8510b
pavel <pavel.sedlacek2004@gmail.com>
2021-12-18 21:06:38 +0100

    2021/18

--- src/main/kotlin/runAll.kt
+++ src/main/kotlin/runAll.kt
@@ -1,5 +1,4 @@
 import y2021.Exec2021
-import y2022.Exec2022
 
 @OptIn(ExperimentalStdlibApi::class)
 fun main() {
@@ -8,6 +7,5 @@ fun main() {
 //        Exec2017,
 //        Exec2020,
         Exec2021,
-//        Exec2022
     ).onEach { it.execute(); repeat(3) { println() } }
 }
